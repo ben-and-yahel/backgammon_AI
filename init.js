0a123d98ca66486422f31efc5875be4e68e6b0ea
yahel manor <yahelmanor20@gmail.com>
2020-08-10 20:06:24 +0300

    half way of move

--- init.js
+++ init.js
@@ -51,6 +51,44 @@ class Tile{
         ctx.closePath();
     }
 }
+class Triangle{
+    constructor(tiles = [], color="black", inverted=false, x = 0, y = 0){
+        this.color = color;
+        this.x = x;
+        this.y = y;
+        this.inverted = inverted;
+        this.glow = false;
+        
+        this.frame_size = 20;
+        this.width = ctx.canvas.width;
+        this.width = this.width/ 15.12903225806452;
+        this.height = 200;
+        this.tiles = tiles;
+        this.length = tiles.length;
+    }
+    draw(){
+        let height_parameter = this.height * Math.cos(Math.PI / 6);
+
+        if (this.inverted) {
+            height_parameter *= -1;
+            this.y = this.frame_size+ 5;
+        }
+        ctx.beginPath();
+        ctx.moveTo(this.x, this.y);
+        ctx.lineTo(this.x+this.width, this.y);
+        ctx.lineTo(this.x+this.width/2, this.y - height_parameter);
+        ctx.closePath();
+
+        // the outline
+        ctx.lineWidth = 10;
+        ctx.strokeStyle = '#666666';
+        ctx.stroke();
+
+        // the fill color
+        ctx.fillStyle = this.glow ? "#666666" : this.color;
+        ctx.fill();
+    }
+}
 
 class Cube{
     constructor(state, seperate_x=0){
@@ -173,8 +211,23 @@ class Cube{
         }
     }
 }
-function move(params) {
-    
+function validMove(number) {
+    if (number > 24 || number < 0) {
+        return false;
+    }
+    return true;
+}
+function move(tiles_x ,tiles_y) {
+    minus = 1;
+    if (currTile.color == "black") {
+        minus = -1;
+    }
+     move3 = tiles_x + (cubes[0].state + cubes[1].state) * minus;
+     if(validMove(move3) == false)
+        return;
+     board[move3].glow = true;
+     board[move3].draw();
+     //draw_triangle(x, y, width, height, color);
 }
 
 function mouseClick(e) {
@@ -182,14 +235,16 @@ function mouseClick(e) {
     let mouse_y = event.clientY;
 
     for (let tiles_x = 0; tiles_x < board.length; tiles_x++) {
-        if(board[tiles_x] == "none")
+        if(board[tiles_x].tiles == [])
             continue;
         for (let tiles_y = 0; tiles_y < board[tiles_x].length; tiles_y++) {
-            if(board[tiles_x][tiles_y] == currTile && currTile.color == "gray")
+            if(board[tiles_x].tiles[tiles_y] == currTile && currTile.color == "gray")
                 continue;
-            if (mouse_x >= board[tiles_x][tiles_y].x - radius && mouse_x <= board[tiles_x][tiles_y].x + radius) {
-            if (mouse_y >= board[tiles_x][tiles_y].y - radius && mouse_y <= board[tiles_x][tiles_y].y + radius) {
+            if (mouse_x >= board[tiles_x].tiles[tiles_y].x - radius && mouse_x <= board[tiles_x].tiles[tiles_y].x + radius) {
+            if (mouse_y >= board[tiles_x].tiles[tiles_y].y - radius && mouse_y <= board[tiles_x].tiles[tiles_y].y + radius) {
 
+
+                move(tiles_x ,tiles_y);
                 currTile.color = "gray";
                 currTile.draw();
                 }
@@ -203,19 +258,19 @@ function mouse(e) {
     let mouse_y = event.clientY;
 
     for (let tiles_x = 0; tiles_x < board.length; tiles_x++) {
-        if(board[tiles_x] == "none")
+        if(board[tiles_x].tiles == [])
             continue;
         for (let tiles_y = 0; tiles_y < board[tiles_x].length; tiles_y++) {
-            if(board[tiles_x][tiles_y] == currTile)
+            if(board[tiles_x].tiles[tiles_y] == currTile)
                 continue;
-            if (mouse_x >= board[tiles_x][tiles_y].x - radius && mouse_x <= board[tiles_x][tiles_y].x + radius) {
-            if (mouse_y >= board[tiles_x][tiles_y].y - radius && mouse_y <= board[tiles_x][tiles_y].y + radius) {
+            if (mouse_x >= board[tiles_x].tiles[tiles_y].x - radius && mouse_x <= board[tiles_x].tiles[tiles_y].x + radius) {
+            if (mouse_y >= board[tiles_x].tiles[tiles_y].y - radius && mouse_y <= board[tiles_x].tiles[tiles_y].y + radius) {
                 console.log("hover");
                 if (currTile.glow == true) {
                     currTile.glow = false;
                     //currTile.draw();
                 }
-                currTile = board[tiles_x][tiles_y];
+                currTile = board[tiles_x].tiles[tiles_y];
                 currTile.glow = true;
                 print_board(board);
                 
@@ -225,47 +280,74 @@ function mouse(e) {
     }
 }
 // ----------------init functions--------------------
-
 function init() {
     //makes the board
-    board.push([new Tile("white", 0), new Tile("white", 0)]);
-    board.push("none");
-    board.push("none");
-    board.push("none");
-    board.push("none");
-    board.push([new Tile("black", 0), new Tile("black", 0), new Tile("black", 0), new Tile("black", 0), new Tile("black", 0)]);
-    board.push("none");
-    board.push([new Tile("black", 0), new Tile("black", 0), new Tile("black", 0)]);
-    board.push("none");
-    board.push("none");
-    board.push("none");
-    board.push([new Tile("white", 0), new Tile("white", 0), new Tile("white", 0), new Tile("white", 0), new Tile("white", 0)]);
-    board.push([new Tile("black", 0), new Tile("black", 0), new Tile("black", 0), new Tile("black", 0), new Tile("black", 0)]);
-    board.push("none");
-    board.push("none");
-    board.push("none");
-    board.push([new Tile("white", 0), new Tile("white", 0), new Tile("white", 0)]);
-    board.push("none");
-    board.push([new Tile("white", 0), new Tile("white", 0), new Tile("white", 0), new Tile("white", 0), new Tile("white", 0)]);
-    board.push("none");
-    board.push("none");
-    board.push("none");
-    board.push("none");
-    board.push([new Tile("black", 0), new Tile("black", 0)]);
+    board.push(new Triangle([new Tile("white", 0), new Tile("white", 0)]));
+    board.push(new Triangle([], "red"));
+    board.push(new Triangle());
+    board.push(new Triangle([], "red"));
+    board.push(new Triangle());
+    board.push(new Triangle([new Tile("black", 0), new Tile("black", 0), new Tile("black", 0), new Tile("black", 0), new Tile("black", 0)], "red"));
+    board.push(new Triangle());
+    board.push(new Triangle([new Tile("black", 0), new Tile("black", 0), new Tile("black", 0)], "red"));
+    board.push(new Triangle());
+    board.push(new Triangle([], "red"));
+    board.push(new Triangle());
+    board.push(new Triangle([new Tile("white", 0), new Tile("white", 0), new Tile("white", 0), new Tile("white", 0), new Tile("white", 0)], "red"));
+    board.push(new Triangle([new Tile("black", 0), new Tile("black", 0), new Tile("black", 0), new Tile("black", 0), new Tile("black", 0)]));
+    board.push(new Triangle([], "red"));
+    board.push(new Triangle());
+    board.push(new Triangle([], "red"));
+    board.push(new Triangle([new Tile("white", 0), new Tile("white", 0), new Tile("white", 0)]));
+    board.push(new Triangle([], "red"));
+    board.push(new Triangle([new Tile("white", 0), new Tile("white", 0), new Tile("white", 0), new Tile("white", 0), new Tile("white", 0)]));
+    board.push(new Triangle([], "red"));
+    board.push(new Triangle());
+    board.push(new Triangle([], "red"));
+    board.push(new Triangle());
+    board.push(new Triangle([new Tile("black", 0), new Tile("black", 0)], "red"));
     
     cubes[1] = new Cube(Math.floor(Math.random() * 6) + 1, 50);
     cubes[0] = new Cube(Math.floor(Math.random() * 6) + 1, 0);
     
+    init_Triangels();
     print_board(board);
+    
 
+}
+frame_size = 20;
+X_seperate = 8;
+function init_Triangels() {
+    let x = frame_size + X_seperate;
+    let y = ctx.canvas.height-frame_size - 5; // 5 for seperation
+    let width =  ctx.canvas.width;
+    width = width/ 15.12903225806452; // TODO : change the formula
+    const height = 200;
+    color = "black";
+    for (let i = 0; i < 12; i++) {
+        board[i].x = x;
+        board[i].y = y;
+        
+        x += width + X_seperate+5;
+        //board[i].color = board[i].color == "black" ? "red" : "black";           
+    }
+    x = frame_size + X_seperate;
+    //color = color == "black" ? "red" : "black";
+    for (let i = 12; i < 24; i++) {
+        board[i].x = x;
+        board[i].y = y;
+        board[i].inverted = true;
+        x += width + X_seperate+5;
+        //board[i].color = board[i].color == "black" ? "red" : "black";      
+    }
 }
 function print_board(board)
 {
     ctx.shadowBlur = 0;//stop glowing effect
     ctx.shadowColor = "black";
     //------------------------drawing the frame----------------
-    frame_size = 20;
-    X_seperate = 8;
+    
+    
     Y_seperate = 15;
     ctx.fillStyle = "#ff9900";
     ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
@@ -283,69 +365,55 @@ function print_board(board)
     ctx.moveTo(0, 0);
     ctx.lineTo(frame_size, frame_size);
     ctx.stroke();
+
     //------------------------drawing the cubes----------------
     cubes[0].draw();
     cubes[1].draw();
 
 
     //------------------------drawing the triangels----------------
-    let x = frame_size + X_seperate;
-    let y = ctx.canvas.height-frame_size - 5; // 5 for seperation
-    let width =  ctx.canvas.width;
-    width = width/ 15.12903225806452; // TODO : change the formula
-    const height = 200;
-    color = "black";
-    for (let i = 0; i < 12; i++) {
-        draw_triangle(x, y, width, height, color);
-        x += width + X_seperate+5;
-        color = color == "black" ? "red" : "black";           
+    
+    for (let i = 0; i < board.length; i++) {
+        board[i].draw();
     }
-    x = frame_size + X_seperate;
-    color = color == "black" ? "red" : "black";
-    for (let i = 0; i < 12; i++) {
-        draw_triangle(x, y, width, height, color, true);
-        x += width + X_seperate+5;
-        color = color == "black" ? "red" : "black";           
-    }
-
 
     //------------------------drawing the tiles----------------
-    radius = width*0.5;
+    radius = board[0].width*0.5;
     x = frame_size + X_seperate + 30;
     y = ctx.canvas.height- frame_size*2 - Y_seperate;//864
     save_j = 0;
     for (let i = 0; i < board.length/2; i++) {
         for (let j = 0; j < board[i].length; j++) {
-            if ( board[i] == "none") {
+            if (board[i].tiles == []) {
                 continue;
             }
-            board[i][j].x = x;
-            board[i][j].y = y + radius*2;
-            board[i][j].draw();
+            board[i].tiles[j].x = x;
+            board[i].tiles[j].y = y + radius*2;
+            board[i].tiles[j].draw();
             //draw_tile(x, y, radius, board[i][j].color);
             y -= radius*2;
            
         }
         y = ctx.canvas.height- frame_size*2 - Y_seperate;
-        x += width + X_seperate + 5;
+        x +=  board[0].width + X_seperate + 5;
     }
     x = frame_size + X_seperate + radius;
     y  = frame_size*2 + Y_seperate;
    
     for (let i = board.length-1; i > (board.length/2)-1; i--) {
         for (let j = 0; j < board[i].length; j++) {
-            if (board[i] == "none") {
+            if (board[i].tiles == []) {
                 continue;
             }
-            board[i][j].x = x;
-            board[i][j].y = y + radius*2;
-            board[i][j].draw();
+            board[i].tiles[j].x = x;
+            board[i].tiles[j].y = y + radius*2;
+            board[i].tiles[j].draw();
             //draw_tile(x, y, radius, board[i][j].color);
            y += radius*2;
            
         }
         y =  frame_size*2 + Y_seperate;
-        x += width + X_seperate + 5;
+        x +=  board[0].width + X_seperate + 5;
     }
     
     
@@ -359,30 +427,4 @@ function role() {
     headline = document.getElementById("welcome");
     headline.innerHTML = "Its the "+turn+" turn!"; 
     turn = turn == "white" ? "black" : "white";
-}
-function draw_tile(x, y, radius, color) {
-    
-}
-function draw_triangle(x, y, width, height, color, inverted=false)
-{
-    let height_parameter = height * Math.cos(Math.PI / 6);
-
-    if (inverted) {
-        height_parameter *= -1;
-        y = frame_size+ 5;
-    }
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.lineTo(x+width, y);
-    ctx.lineTo(x+width/2, y - height_parameter);
-    ctx.closePath();
-
-    // the outline
-    ctx.lineWidth = 10;
-    ctx.strokeStyle = '#666666';
-    ctx.stroke();
-
-    // the fill color
-    ctx.fillStyle = color;
-    ctx.fill();
 }
