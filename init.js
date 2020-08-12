8cd25f0046c90a0cd780feb5efc49aba84a546e8
yahel manor <yahelmanor20@gmail.com>
2020-08-12 20:42:14 +0300

    done with move function(need code formating)

--- init.js
+++ init.js
@@ -279,9 +279,15 @@ function validMove(number) {
     }
     return true;
 }
-function move(tiles_x ,tiles_y) {
+/**
+ * 
+ * @param {the position of the Tile} tiles_x 
+ * the function is called once the player clicks on tile and wants to view his move options
+ */
+function move(tiles_x ) {
     minus = 1;
-    if (currTile.color == "black" && tiles_x <12) 
+    //In some scenarios we need to reverse the calaculation of the move
+    if ((currTile.color == "black" && tiles_x <12 || currTile.color == "white" && tiles_x >12)) 
         minus = -1;
     succseed = 0;
     moves = [];
@@ -289,8 +295,11 @@ function move(tiles_x ,tiles_y) {
     moves[1] = tiles_x + (cubes[1].state * minus);
     moves[2] = tiles_x + ((cubes[0].state + cubes[1].state)* minus);
     for (let i = 0; i < moves.length; i++) {
-        // if (moves[i] > 11 && moves[i] < 24 && minus == 1)
-        //     moves[i] = 23 - (moves[i] % 12); 
+         //In some scenario we need to modulo the cubes and reverse them
+        if (moves[i] > 11 && currTile.color == "white" && minus == 1)
+            moves[i] = 23 - (moves[i] % 12); 
+        if (moves[i] > 23 && currTile.color == "black" && minus == 1)
+            moves[i] = 11 - (moves[i] % 12);
 
         if(validMove(moves[i]) == false)
             continue;
@@ -302,17 +311,38 @@ function move(tiles_x ,tiles_y) {
     }
 }
 
-function mouseClick(e) {
-    let mouse_x = event.clientX;
-    let mouse_y = event.clientY;
-console.log(mouse_x,mouse_y)
+//returns a the loacation of the sign'ed tile for moving it.
+function find_sign_tile() {
     for (let tiles_x = 0; tiles_x < board.length; tiles_x++) {
         if(board[tiles_x].tiles == [])
             continue;
+        for (let tiles_y = 0; tiles_y < board[tiles_x].length; tiles_y++) {
+            if(board[tiles_x].tiles[tiles_y].sign == true)
+                return [tiles_x, tiles_y];
+                
+        }
+    }
+}
+function mouseClick(e) {
+    let mouse_x = event.clientX;
+    let mouse_y = event.clientY;
+    for (let tiles_x = 0; tiles_x < board.length; tiles_x++) {
+        if(board[tiles_x].tiles == [])
+            continue;
+        //TODO: doc this!!
         if(board[tiles_x].sign && board[tiles_x].isInside(mouse_x,mouse_y - 5 - board[tiles_x].frame_size - strap_height))
         {
-            alert("move");
+            let tiles_loc = find_sign_tile();
+            board[tiles_x].tiles.push(board[tiles_loc[0]].tiles[tiles_loc[1]]);
+            board[tiles_x].length += 1;
+            //board[tiles_x].tiles[tiles_loc[1]].sign = false;
+
+            board[tiles_loc[0]].tiles.splice(tiles_loc[1], 1); // delets the old tile
+            board[tiles_loc[0]].length -= 1;
             clean();
+            break;
+            //alert("move");
+            
         }
         for (let tiles_y = 0; tiles_y < board[tiles_x].length; tiles_y++) {
             if(board[tiles_x].tiles[tiles_y] == currTile && currTile.color == "gray")
@@ -321,7 +351,7 @@ console.log(mouse_x,mouse_y)
             if (mouse_y >= board[tiles_x].tiles[tiles_y].y - radius && mouse_y <= board[tiles_x].tiles[tiles_y].y + radius) {
 
                 clean();
-                move(tiles_x ,tiles_y);
+                move(tiles_x); // needes to get tiles_x for the calc
                 currTile.sign = true;
                 currTile.draw();
                 }
